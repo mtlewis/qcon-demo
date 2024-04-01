8602124e9960f1ac956123e4c23b62727d38a956
MT Lewis <mtlewis@users.noreply.github.com>
2024-04-01 21:33:04 +0100

    Ingest planets from API

--- plugins/catalog-backend-module-planets/src/module.ts
+++ plugins/catalog-backend-module-planets/src/module.ts
@@ -12,7 +12,7 @@ import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/
 const PROVIDER_NAME = 'planets';
 const PLANETS_API = 'http://localhost:8000/api/planets.json';
 
-const toResourceEntity = (name: string, image: string) => ({
+const toResourceEntity = (name: string, image: string, secrecy: string) => ({
   apiVersion: 'backstage.io/v1alpha1',
   kind: 'Resource',
   metadata: {
@@ -21,6 +21,7 @@ const toResourceEntity = (name: string, image: string) => ({
       [ANNOTATION_LOCATION]: `url:${PLANETS_API}`,
       [ANNOTATION_ORIGIN_LOCATION]: `url:${PLANETS_API}`,
       image: new URL(image, PLANETS_API),
+      secrecy,
     },
   },
   spec: {
@@ -35,10 +36,11 @@ export const catalogBackendModulePlanets = createBackendModule({
   register(reg) {
     reg.registerInit({
       deps: {
-        catalogProcessing: catalogProcessingExtensionPoint,
+        logger: coreServices.logger,
         scheduler: coreServices.scheduler,
+        catalogProcessing: catalogProcessingExtensionPoint,
       },
-      async init({ catalogProcessing, scheduler }) {
+      async init({ catalogProcessing, logger, scheduler }) {
         catalogProcessing.addEntityProvider({
           getProviderName: () => PROVIDER_NAME,
           connect: async connection => {
@@ -47,15 +49,24 @@ export const catalogBackendModulePlanets = createBackendModule({
               frequency: { seconds: 5 },
               timeout: { seconds: 1 },
               fn: async () => {
+                logger.info(`Refreshing planet resources...`);
                 const planetsResponse = await fetch(PLANETS_API);
                 const planets = await planetsResponse.json();
 
                 await connection.applyMutation({
                   type: 'full',
                   entities: planets.map(
-                    ({ name, image }: { name: string; image: string }) => ({
+                    ({
+                      name,
+                      image,
+                      secrecy,
+                    }: {
+                      name: string;
+                      image: string;
+                      secrecy: string;
+                    }) => ({
                       locationKey: PROVIDER_NAME,
-                      entity: toResourceEntity(name, image),
+                      entity: toResourceEntity(name, image, secrecy),
                     }),
                   ),
                 });
